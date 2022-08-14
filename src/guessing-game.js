fa0dd96ae27f6b49f73943cce412f8e818a6621d
Jakhongir <jakhongir.nishanbaev@gmail.com>
2022-08-14 11:08:10 +0500

    feat: implement binary search algorithm

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
+        this.candidate = this.min + Math.ceil((this.max - this.min) / 2);
 
+        console.log('min=', this.min, 'max=', this.max);
+
+        return this.candidate;
     }
 
     lower() {
-
+        this.max = this.candidate;
     }
 
     greater() {
-
+        this.min = this.candidate;
     }
 }
 
